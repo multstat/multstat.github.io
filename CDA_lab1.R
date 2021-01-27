79c8fc207a3c50141a8ff4465e49772941dc7702
multstat <54950941+multstat@users.noreply.github.com>
2021-01-28 01:03:52 +0300

    Add files via upload

--- CDA_lab1.R
+++ CDA_lab1.R
@@ -1,26 +1,20 @@
 ### Categorical Data Analysis
-### Lab 1. January 24, 2020
+### Lab 1. January 26, 2021
 ### Binary response models. Part 1: estimation and interpretation
 
 install.packages("haven") 
 install.packages("psych") 
 install.packages("dplyr") 
 install.packages("ggplot2")
-install.packages("lmtest")
-install.packages("aod")
-install.packages("multcomp")
 install.packages("glm.predict")
 install.packages("questionr")
-install.packages("DAMisc") ### Для загрузки могут понадобиться еще пакеты car, effects, rstan, QRM, StanHeaders
+install.packages("DAMisc")
 install.packages("erer")
 
 library(haven) 
 library(psych)
 library(dplyr)
 library(ggplot2)
-library(lmtest)
-library(aod)
-library(multcomp)
 library(glm.predict)
 library(questionr)
 library(DAMisc)
@@ -72,7 +66,7 @@ lits$measures[lits$q804a == 1 | lits$q804g == 1 | lits$q804j == 1| lits$q804l ==
 table(lits$measures)
 
 table(lits$q228)
-lits$q228[(lits$q228==-97 | lits$q228==-99)] <- NA
+lits$q228[lits$q228==-97 | lits$q228==-99] <- NA
 
 table(lits$respondentage)
 lits$respondentage[lits$respondentage==-1] <- NA
@@ -89,68 +83,54 @@ summary(logit)
 range <- data.frame(respondentage=seq(18, 93, 1))
 pred.logit <- predict(logit, range, type="response")
 pred.data <- mutate(range, pred.logit)
+pred.data
 summary(pred.logit)
 
-ggplot(pred.data, aes(x=respondentage, y=pred.logit)) +  
-  geom_line(data=pred.data, aes(y=pred.logit)) +
+ggplot(pred.data, aes(x=respondentage, y=pred.logit)) +  geom_line() +
   labs(x = "Age", y = "Predicted Probability", title = "Predicted Probability of Taking Measures During the Crisis") + theme_light()
 
 m1_logit <- glm(measures ~ gender + respondentage + as.factor(settlement) + Children + ladder, data = lits, 
-                family = binomial(link = "logit"))
+             family = binomial(link = "logit"))
 summary(m1_logit)
 
 m1_probit <- glm(measures ~ gender + respondentage + as.factor(settlement) + Children + ladder, data = lits, 
-                 family = binomial(link = "probit"))
+                family = binomial(link = "probit"))
 summary(m1_probit)
 
 # compare the results
 round(data.frame(logit = m1_logit$coefficients, probit = m1_probit$coefficients, 
                  quotient = m1_logit$coefficients/m1_probit$coefficients), 3)
 
-# Wald test. Single coefficient 
-wald.test(b = m1_logit$coefficients, Sigma = vcov(m1_logit), Terms = 2) #Gender
-wald.test(b = m1_logit$coefficients, Sigma = vcov(m1_logit), Terms = 4) # Rural settlement
-wald.test(b = m1_logit$coefficients, Sigma = vcov(m1_logit), Terms = 6) #Children
-
-# Wald test. Multiple coefficients
-wald.test(b = m1_logit$coefficients, Sigma = vcov(m1_logit), Terms = 4:5)
-
-# Likelihood-ratio test 
-m0_logit <- glm(measures ~ 1, data = lits, family = binomial(link = "logit"))
-lrtest(m0_logit, m1_logit)
-
 # Prediction and interpretation
 lits <- mutate(lits, rural = ifelse(lits$settlement == 2, 1, 0))
 lits <- mutate(lits, metropolitan = ifelse(lits$settlement == 3, 1, 0))
 m1_logit_ <- glm(measures ~ gender + respondentage + rural + metropolitan + Children + ladder, data = lits, 
-                 family = binomial(link = "logit"), x = TRUE) 
+                family = binomial(link = "logit"), x = TRUE) 
 summary(m1_logit_)
 # x = TRUE is needed to compute the marginal effects
 
-
 lits <- mutate(lits, pred = m1_logit_$fitted.values)
+
 ggplot(lits, aes(pred)) + geom_histogram(binwidth=0.03, color="black",
-                                         fill="violet") + theme_bw()
+                                          fill="violet") + theme_bw()
 
 basepredict(m1_logit_, c(1, 1, mean(lits$respondentage), 0, 0, 0, mean(lits$ladder)))
 basepredict(m1_logit_, c(1, 0, mean(lits$respondentage), 0, 0, 0, mean(lits$ladder)))
 
 ### Discrete Change in predicted probabilities
 dc(m1_logit_, values1 = c(1, 1, mean(lits$respondentage), 0, 0, 0, mean(lits$ladder)),
-   values2 = c(1, 1, mean(lits$respondentage), 0, 0, 1, mean(lits$ladder)))
+   values2 = c(1, 0, mean(lits$respondentage), 0, 0, 1, mean(lits$ladder)))
 
-dc(m1_logit_, values1 = c(1, 1, mean(lits$respondentage), 0, 0, 0, min(lits$ladder)),
-   values2 = c(1, 1, mean(lits$respondentage), 0, 0, 0, max(lits$ladder)))
+dc(m1_logit_, values1 = c(1, 1, mean(lits$respondentage), 0, 0, 0, max(lits$ladder)),
+   values2 = c(1, 1, mean(lits$respondentage), 0, 0, 0, min(lits$ladder)))
 
 ### Discrete change for many variables
-glmChange(m1_logit_, data = lits, typical.dat=NULL, diffchange=c("range"), sim=T, R=1000) # min->max
-glmChange(m1_logit_, data = lits, typical.dat=NULL, diffchange=c("sd"), sim=T, R=1000)    # -+std.deviation/2
+glmChange(m1_logit_, data = lits, diffchange=c("range")) # min->max
 
 ### Marginal effects
 maBina(m1_logit_)                 # marginal effects at the means of independent variables
 maBina(m1_logit_, x.mean = FALSE) # marginal effects are calculated for each observation and then averaged
 
-# Odds ratio
+### Odds ratio
 summary(m1_logit_)
-exp(cbind(m1_logit_$coefficients, confint(m1_logit_)))
 odds.ratio(m1_logit_, level = 0.95, signif.stars = T)
